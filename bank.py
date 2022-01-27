ba4ee763134037509d364f23696d737fe8e47eac
Odiseja <98523466+Odiseja@users.noreply.github.com>
2022-01-27 13:09:38 +0200

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,35 @@
+import datetime
+from multiprocessing.connection import Client
+
+class Account:
+    auto_account_number = 1234567890
+
+    def __init__(self, account_number):
+       self.account_number = Account.auto_account_number
+       self.currency = currency
+        self.initial_balance = datetime.datetime.now()
+
+
+        c1 = Client('Anna')
+        #print(f'{c1.name} {c1.timestamp')
+
+        a1 = Account('EUR', 200)
+        print(f'{a1.account_number} {a1.currency} {a1.initial_balance} {a1.timestamp}')
+        a2 = Account('USD', 150)
+        a3 = Account('PLN', 400)
+
+        def add_account(self, account:Account):
+            self.accounts.append(account)
+
+            c1 = Client('Anna')
+
+
+c[]
+c.append('Anna')
+c.append
+
+
+clients[0].add_account(Client('EUR', 800))
+clients[0].add_account(Client('USD', 150))
+
+    
